76f41b4402dbb7c7d491fdced8a6e88bdb618baf
ramos3 <95050078+ramos3@users.noreply.github.com>
2025-06-11 17:50:20 -0500

    Adding dependencies and initial HourlyEmployee test setup
    Adding dependencies and initial HourlyEmployee test setup

---
+++ src/test/java/EmployeeObjects/HourlyEmployeeTest.java
@@ -0,0 +1,14 @@
+package EmployeeObjects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HourlyEmployeeTest
+{
+    HourlyEmployee emp = new HourlyEmployee("Tony", "Stark", 5749, "Service", "Lead Service Manager", 32.85);
+
+    
+
+
+
+
+}
