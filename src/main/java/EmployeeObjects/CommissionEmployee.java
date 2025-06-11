4d7950335c19f145ac504666850a93ab713d4d13
ramos3 <95050078+ramos3@users.noreply.github.com>
2025-06-11 18:55:19 -0500

    Test failed so added condition for positive numbers only

--- src/main/java/EmployeeObjects/CommissionEmployee.java
+++ src/main/java/EmployeeObjects/CommissionEmployee.java
@@ -43,7 +43,10 @@ public final class CommissionEmployee extends Employee {
      */
     public void increaseSales(double s)
     {
-        sales += s;
+        if (s > 0)
+        {
+            sales += s;
+        }
     }
 
     /**
